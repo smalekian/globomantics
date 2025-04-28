46ac809ac519c614c3a81e3bcc7d52e50b4339d8
Spencer Malekian <spencer.malekian@gmail.com>
2025-04-28 16:30:37 -0400

    removes footer image

--- app/src/Footer.tsx
+++ app/src/Footer.tsx
@@ -31,15 +31,10 @@ export function Footer() {
       </div>
 
       <div className="video-sec col-md-4 col-sm-12 col-xs-12">
-        <h1 className="video-heading text-center">
-          <img src="images/pluralsight-white.png" alt="" />
-        </h1>
-        <span className="border"></span>
         <p className="footer-p">
           This site is created for demonstrative purposes only and does not offer any real products
           or services.
         </p>
-        <p></p>
       </div>
     </footer>
   );
